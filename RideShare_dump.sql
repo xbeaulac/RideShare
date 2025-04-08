b343261c28537a080146cbe7a614ac510021dbc8
Maks Popov <112341327+00mkp@users.noreply.github.com>
2025-04-07 19:07:55 -0700

    SQL Dump
    Contains table definitions and test data.

---
+++ RideShare_dump.sql
@@ -0,0 +1,110 @@
+-- MySQL dump 10.13  Distrib 9.2.0, for macos14.7 (arm64)
+--
+-- Host: localhost    Database: RideShare
+-- ------------------------------------------------------
+-- Server version	9.2.0
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Driver`
+--
+
+DROP TABLE IF EXISTS `Driver`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Driver` (
+  `username` varchar(20) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Driver`
+--
+
+LOCK TABLES `Driver` WRITE;
+/*!40000 ALTER TABLE `Driver` DISABLE KEYS */;
+INSERT INTO `Driver` VALUES ('anasp','Anastasia','Prokofieva',0),('cocopla','Constantine','Plaskovich',1),('jdoe01','Jane','Doe',0),('jgibbs','Jethro','Gibbs',0),('sergzh','Sergei','Zhukov',1),('vzlow','Victor','Zlowakovic',1);
+/*!40000 ALTER TABLE `Driver` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Ride`
+--
+
+DROP TABLE IF EXISTS `Ride`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Ride` (
+  `rideID` int NOT NULL AUTO_INCREMENT,
+  `rider` varchar(20) NOT NULL,
+  `driver` varchar(20) NOT NULL,
+  `pickUpLocation` varchar(100) NOT NULL,
+  `dropOfflocation` varchar(100) NOT NULL,
+  `rating` int DEFAULT NULL,
+  PRIMARY KEY (`rideID`),
+  KEY `rider` (`rider`),
+  KEY `driver` (`driver`),
+  CONSTRAINT `ride_ibfk_1` FOREIGN KEY (`rider`) REFERENCES `Rider` (`username`),
+  CONSTRAINT `ride_ibfk_2` FOREIGN KEY (`driver`) REFERENCES `Driver` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Ride`
+--
+
+LOCK TABLES `Ride` WRITE;
+/*!40000 ALTER TABLE `Ride` DISABLE KEYS */;
+INSERT INTO `Ride` VALUES (1,'mkp00','jdoe01','5 Street Lane','100 Road Way',5),(2,'joreese','jdoe01','10 Avenue Street','915 E Way Court',4);
+/*!40000 ALTER TABLE `Ride` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Rider`
+--
+
+DROP TABLE IF EXISTS `Rider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Rider` (
+  `username` varchar(20) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Rider`
+--
+
+LOCK TABLES `Rider` WRITE;
+/*!40000 ALTER TABLE `Rider` DISABLE KEYS */;
+INSERT INTO `Rider` VALUES ('fbrdtt','Franklin','Burdett'),('jdoe00','John','Doe'),('joreese','John','Reese'),('lgrab','Lemon','Grab'),('mkp00','Maksim','Popov'),('zmorgan','Zoe','Morgan');
+/*!40000 ALTER TABLE `Rider` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-04-07 19:03:30
