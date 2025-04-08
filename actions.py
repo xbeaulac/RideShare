b79176635aec00ba2507aab5571eef220b1716ef
Xander Beaulac <xbeaulac@gmail.com>
2025-04-08 12:38:19 -0700

    cli onboarding

--- actions.py
+++ actions.py
@@ -15,9 +15,32 @@ class Database:
     def createRider(self, username, first_name, last_name):
         query = ("INSERT INTO Rider "
                  "(username, firstName, lastName) "
-                 "VALUES (%s %s %s)")
+                 "VALUES (%s, %s, %s)")
         self.cursor.execute(query, (username, first_name, last_name))
         self.connection.commit()
+
+    def createDriver(self, username, first_name, last_name):
+        query = ("INSERT INTO Driver "
+                 "(username, firstName, lastName, active) "
+                 "VALUES (%s, %s, %s, TRUE)")
+        self.cursor.execute(query, (username, first_name, last_name))
+        self.connection.commit()
+
+    def signInRider(self, username):
+        query = ("SELECT * "
+                 "FROM Rider "
+                 "WHERE username = %s")
+        self.cursor.execute(query, [username])
+        self.connection.commit()
+        return self.cursor.fetchone()
+
+    def signInDriver(self, username):
+        query = ("SELECT * "
+                 "FROM Driver "
+                 "WHERE username = %s")
+        self.cursor.execute(query, [username])
+        self.connection.commit()
+        return self.cursor.fetchone()
 
     def close(self):
         self.cursor.close()
--- hello.py
+++ hello.py
@@ -2,35 +2,51 @@ from actions import *
 
 db = Database()
 
-# print("Welcome to RideShare! ")
-# user_input = input("Are you a new user (0) or returning user (1): ")
-# if user_input == "0":
-#     print("You selected: new user.")
-#     user_input = input("Create a rider (0) or driver (1) account: ")
-#     if user_input == "0":
-#         print("You selected: rider.")
-#         username = input("Enter username: ")
-#         first_name = input("Enter first name: ")
-#         last_name = input("Enter last name: ")
-#         db.createRider(username, first_name, last_name)
-#     if user_input == "1":
-#         print("You selected: driver.")
-#         username = input("Enter username: ")
-#         first_name = input("Enter first name: ")
-#         last_name = input("Enter last name: ")
-# if user_input == "1":
-#     print("You selected: returning user.")
-#     user_input = input("Log in to a rider (0) or driver (1) account: ")
-#     if user_input == "0":
-#         print("You selected: rider.")
-#         user_input = input("Enter rider username: ")
-#     if user_input == "1":
-#         print("You selected: driver.")
-#         user_input = input("Enter driver username: ")
+print("Welcome to RideShare! ")
+username = None
+user_input = input("Are you a new user (0) or returning user (1): ")
+if user_input == "0":
+    print("You selected: new user.")
+    user_input = input("Create a rider (0) or driver (1) account: ")
+    if user_input == "0":
+        print("You selected: rider.")
+        username = input("Enter username: ")
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        db.createRider(username, first_name, last_name)
+    elif user_input == "1":
+        print("You selected: driver.")
+        username = input("Enter username: ")
+        first_name = input("Enter first name: ")
+        last_name = input("Enter last name: ")
+        db.createDriver(username, first_name, last_name)
+elif user_input == "1":
+    print("You selected: returning user.")
+    user_input = input("Log in to a rider (0) or driver (1) account: ")
+    if user_input == "0":
+        print("You selected: rider.")
+        while True:
+            username = input("Enter rider username: ")
+            res = db.signInRider(username)
+            if res is None:
+                print("That username does not exist.")
+                continue
+            else:
+                print(f"Welcome, {res[1]}!")
+                break
+    elif user_input == "1":
+        print("You selected: driver.")
+        while True:
+            username = input("Enter rider username: ")
+            res = db.signInDriver(username)
+            if res is None:
+                print("That username does not exist.")
+                continue
+            else:
+                print(f"Welcome, {res[1]}!")
+                break
 
-db.cursor.execute("SELECT * FROM Rider")
-for (username, firstName, lastName) in db.cursor:
-    print(username, firstName, lastName)
+print("username:", username)
 
 # Close connection
 db.close()
